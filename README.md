fad09258899c1ab8056b9481fb1c62f89c3f7989
Dan Cecile <dancecile@gmail.com>
2018-02-11 14:09:51 +0800

    Initial Elm app 🏗️
    Bootstrapped using Create Elm App.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dan Cecile's resume + portfolio site
+
+## Developing
+
+1. Install [Create Elm App](https://github.com/halfzebra/create-elm-app)
+2. Run `elm-app start` to start the development server
+3. Change a source file, and the app will reload automatically
+
+For other operations, see the [Create Elm App README](https://github.com/halfzebra/create-elm-app/tree/master/template).
+
+## License
+
+This project is released under the MIT License (see
+[LICENSE.md](LICENSE.md) for details).
---
+++ src/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h1, a)
+import Html.Attributes exposing (href)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Dan Cecile" ]
+        , a [ href "https://github.com/dcecile" ] [ text "GitHub" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
