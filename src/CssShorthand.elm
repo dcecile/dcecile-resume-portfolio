951e8be4f4fc6d13bb6af51209cf3cdf6c8f8d6b
Dan Cecile <dancecile@gmail.com>
2018-02-20 15:48:46 +0800

    Style intro screen whitespace 🎨
    For #2

--- src/CssShorthand.elm
+++ src/CssShorthand.elm
@@ -3,12 +3,13 @@ module CssShorthand
         ( animation
         , displayFlexColumn
         , displayFlexRow
+        , displayFlexRowReverse
         , marginRightLeft
         , paddingRightLeft
         , paddingTopBottom
         )
 
-import Css exposing (Length, LengthOrAuto, Style, batch, column, displayFlex, flexDirection, marginLeft, marginRight, paddingLeft, paddingRight, property, row, paddingTop, paddingBottom)
+import Css exposing (Length, LengthOrAuto, Style, batch, column, displayFlex, flexDirection, marginLeft, marginRight, paddingBottom, paddingLeft, paddingRight, paddingTop, property, row, rowReverse)
 
 
 animation : String -> Style
@@ -18,22 +19,27 @@ animation =
 
 displayFlexColumn : Style
 displayFlexColumn =
-    batch [ displayFlex , flexDirection column ]
+    batch [ displayFlex, flexDirection column ]
 
 
 displayFlexRow : Style
 displayFlexRow =
-    batch [ displayFlex , flexDirection row ]
+    batch [ displayFlex, flexDirection row ]
+
+
+displayFlexRowReverse : Style
+displayFlexRowReverse =
+    batch [ displayFlex, flexDirection rowReverse ]
 
 
 paddingRightLeft : Length compatible units -> Style
 paddingRightLeft =
     batchMap [ paddingRight, paddingLeft ]
 
+
 paddingTopBottom : Length compatible units -> Style
 paddingTopBottom =
     batchMap [ paddingTop, paddingBottom ]
-
 
 
 marginRightLeft : LengthOrAuto compatible -> Style
--- src/View/Intro.elm
+++ src/View/Intro.elm
@@ -3,8 +3,8 @@ module View.Intro
         ( viewIntro
         )
 
-import Css exposing (alignItems, bold, center, color, em, fontSize, fontStyle, fontWeight, height, hex, italic, justifyContent, lineHeight, normal, num, opacity, px, vh, width)
-import CssShorthand exposing (displayFlexColumn, displayFlexRow, marginRightLeft, paddingRightLeft)
+import Css exposing (Style, alignItems, alignSelf, bold, center, color, em, flexBasis, flexEnd, flexGrow, flexStart, fontSize, fontStyle, fontWeight, height, hex, italic, justifyContent, lineHeight, marginBottom, marginTop, maxWidth, normal, num, opacity, px, stretch, vh, width, zero)
+import CssShorthand exposing (displayFlexColumn, displayFlexRow, displayFlexRowReverse, marginRightLeft, paddingRightLeft)
 import Html.Styled exposing (Html, a, div, h1, h2, p, span, styled, text)
 import Html.Styled.Attributes exposing (href)
 import HtmlShorthand exposing (HtmlTag, hrefHash, onClickPreventDefault, targetBlank)
@@ -19,7 +19,9 @@ viewIntro model =
     let
         style =
             [ displayFlexColumn
-            , height (vh 80)
+            , justifyContent center
+            , alignItems center
+            , height (vh 85)
             ]
     in
     styled div
@@ -37,8 +39,11 @@ viewName : Html Msg
 viewName =
     let
         style =
-            [ fontWeight normal
+            [ marginTop zero
+            , marginBottom zero
+            , fontWeight normal
             , fontSize <| em 2.2
+            , lineHeight <| num 1.4
             ]
     in
     styled h1
@@ -51,7 +56,9 @@ viewTitle : Html Msg
 viewTitle =
     let
         style =
-            [ fontWeight normal
+            [ marginTop zero
+            , marginBottom <| em 1.4
+            , fontWeight normal
             , fontSize <| em 1
             , fontStyle italic
             ]
@@ -65,6 +72,12 @@ viewTitle =
 viewSellingPoint : Html Msg
 viewSellingPoint =
     let
+        style =
+            [ marginTop zero
+            , marginBottom <| em 1.8
+            , maxWidth <| em 22
+            ]
+
         highlightStyle =
             [ color <| hex "#00a934"
             ]
@@ -77,7 +90,8 @@ viewSellingPoint =
                 >> List.singleton
                 >> styled span highlightStyle []
     in
-    p
+    styled p
+        style
         []
         [ normal "10 years of experience learning new "
         , highlight "tech"
@@ -93,7 +107,10 @@ viewPitch : Html Msg
 viewPitch =
     let
         style =
-            [ fontWeight bold ]
+            [ marginTop zero
+            , marginBottom <| em 1.2
+            , fontWeight bold
+            ]
     in
     styled p
         style
@@ -106,25 +123,46 @@ viewCallsToAction model =
     let
         style =
             [ displayFlexRow
-            , justifyContent center
-            , alignItems center
+            , justifyContent stretch
+            , alignSelf stretch
             ]
     in
     styled div
         style
         []
-        [ viewCallToActionIcon model mailBackground
-        , viewCallToActionButton
+        [ viewCallToAction model
+            displayFlexRow
+            mailBackground
             [ href "mailto:dancecile@gmail.com?subject=Hi"
             , targetBlank
             ]
             [ text "Send me an email" ]
-        , viewCallToActionButton
+        , viewCallToAction model
+            displayFlexRowReverse
+            printerBackground
             [ hrefHash
             , onClickPreventDefault Print
             ]
             [ text "Print my resume" ]
-        , viewCallToActionIcon model printerBackground
+        ]
+
+
+viewCallToAction : Model -> Style -> IconBackground -> HtmlTag Msg
+viewCallToAction model displayStyle iconBackground attributes elements =
+    let
+        style =
+            [ displayStyle
+            , justifyContent flexEnd
+            , alignItems center
+            , flexBasis <| em 0
+            , flexGrow <| num 1
+            ]
+    in
+    styled div
+        style
+        []
+        [ viewCallToActionIcon model iconBackground
+        , viewCallToActionButton attributes elements
         ]
 
 
@@ -138,8 +176,8 @@ viewCallToActionIcon model iconBackground =
             [ iconBackground model.iconSource
             , width iconSize
             , height iconSize
-            , marginRightLeft <| px 2
-            , opacity <| num 0.8
+            , marginRightLeft <| px 8
+            , opacity <| num 0.93
             ]
     in
     styled span
@@ -152,9 +190,9 @@ viewCallToActionButton : HtmlTag Msg
 viewCallToActionButton =
     let
         style =
-            [ marginRightLeft <| px 14
-            , paddingRightLeft <| em 0.8
-            , lineHeight <| em 2.2
+            [ marginRightLeft <| px 18
+            , paddingRightLeft <| em 1.1
+            , lineHeight <| num 2.4
             , Button.border
             , Button.text
             ]
