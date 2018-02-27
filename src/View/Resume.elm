a55138a92542b585718540c0a37f951fa3579a3a
Dan Cecile <dancecile@gmail.com>
2018-02-27 15:50:20 +0800

    Set up basic resume boxes and headers 🏗️
    For #11

--- src/View/Resume.elm
+++ src/View/Resume.elm
@@ -3,13 +3,15 @@ module View.Resume
         ( viewResume
         )
 
-import Css exposing (color, em, flexGrow, justifyContent, num, padding2, spaceBetween)
-import CssShorthand exposing (displayFlexColumn)
-import Html.Styled exposing (Html, a, div, h1, h2, styled, text)
+import Css exposing (color, em, flexBasis, flexGrow, lineHeight, num, padding2, right, textAlign)
+import CssShorthand exposing (displayFlexColumn, displayFlexRow)
+import Data.Basic exposing (BasicData)
+import Html.Styled exposing (Html, a, div, footer, h1, h2, header, main_, nav, p, section, styled, text)
 import Html.Styled.Attributes exposing (href)
 import Model exposing (Model)
 import Msg exposing (Msg)
 import View.Colors exposing (printBlack)
+import View.Metrics exposing (standardLineHeight)
 
 
 viewResume : Model -> Html Msg
@@ -20,35 +22,140 @@ viewResume model =
 
         style =
             [ displayFlexColumn
-            , justifyContent spaceBetween
             , padding2 (em 3) (em 3)
             , flexGrow <| num 1
+            , lineHeight <| num standardLineHeight
             , color printBlack
             ]
     in
-    styled div
+    styled header
         style
         []
-        [ viewHeader basicData.name
+        [ viewHeader basicData
+        , viewMain
         , viewFooter basicData.sourceURL
         ]
 
 
-viewHeader : String -> Html Msg
-viewHeader name =
-    div
+viewHeader : BasicData -> Html Msg
+viewHeader basicData =
+    let
+        style =
+            [ displayFlexRow
+            ]
+    in
+    styled header
+        style
+        []
+        [ viewPrimary basicData.name
+        , viewContact basicData.emailAddress
+        ]
+
+
+viewPrimary : String -> Html Msg
+viewPrimary name =
+    let
+        style =
+            [ displayFlexColumn
+            , flexGrow <| num 1
+            ]
+    in
+    styled header
+        style
         []
         [ h1 [] [ text name ]
-        , h2 [] [ text "Software developer (10 years experience)" ]
-        , a
-            [ href "https://github.com/dcecile" ]
-            [ text "GitHub: https://github.com/dcecile" ]
+        , p [] [ text "Software developer (10 years experience)" ]
+        ]
+
+
+viewContact : String -> Html Msg
+viewContact emailAddress =
+    let
+        style =
+            [ displayFlexColumn
+            , textAlign right
+            ]
+    in
+    styled nav
+        style
+        []
+        [ a
+            [ href <| "mailto:" ++ emailAddress ]
+            [ text emailAddress ]
+        ]
+
+
+viewMain : Html Msg
+viewMain =
+    let
+        style =
+            [ displayFlexRow
+            , flexGrow <| num 1
+            ]
+    in
+    styled main_
+        style
+        []
+        [ viewInfo
+        , viewHistory
+        ]
+
+
+viewInfo : Html Msg
+viewInfo =
+    let
+        style =
+            [ displayFlexColumn
+            , flexBasis <| em 0
+            , flexGrow <| num 1
+            ]
+    in
+    styled section
+        style
+        []
+        [ h2 [] [ text "Proficiencies" ]
+        , p [] []
+        , h2 [] [ text "Side projects" ]
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , h2 [] [ text "Education" ]
+        ]
+
+
+viewHistory : Html Msg
+viewHistory =
+    let
+        style =
+            [ displayFlexColumn
+            , flexBasis <| em 0
+            , flexGrow <| num 1.4
+            ]
+    in
+    styled section
+        style
+        []
+        [ h2 [] [ text "Work history" ]
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , p [] []
+        , h2 [] [ text "Volunteering" ]
         ]
 
 
 viewFooter : String -> Html Msg
 viewFooter sourceURL =
-    div
+    footer
         []
         [ a
             [ href sourceURL ]
