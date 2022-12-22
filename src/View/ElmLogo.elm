05c5e4d147f99e3425beda7a92602bc6685d1090
Dan Cecile <dancecile@gmail.com>
2022-12-21 22:46:46 -0500

    Update Elm logo ♻️

--- src/View/ElmLogo.elm
+++ src/View/ElmLogo.elm
@@ -1,38 +1,35 @@
 module View.ElmLogo exposing (viewElmLogo)
 
 import Css exposing (em, marginBottom, marginLeft)
-import CssShorthand exposing (batchMap, filter)
-import Svg.Styled exposing (Svg, polygon, rect, styled, svg)
-import Svg.Styled.Attributes exposing (fill, height, points, transform, viewBox, width, x, y)
+import CssShorthand exposing (batchMap)
+import Svg.Styled exposing (Svg, g, polygon, styled, svg)
+import Svg.Styled.Attributes exposing (fill, points, transform, viewBox)
 
 
 viewElmLogo : Svg msg
 viewElmLogo =
     let
         style =
-            [ batchMap [ Css.width, Css.height ] <| em 0.8
+            [ batchMap [ Css.width, Css.height ] <| em 0.9
             , marginBottom <| em -0.1
             , marginLeft <| em 0.1
-            , filter "grayscale(30%) opacity(70%)"
             ]
     in
     styled svg
         style
-        [ viewBox "0 0 323.141 322.95"
+        [ viewBox "-300 -300 600 600"
         ]
-        [ polygon [ fill "#F0AD00", points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
-        , polygon [ fill "#7FD13B", points "8.867,0 79.241,70.375 232.213,70.375 161.838,0" ] []
-        , rect
-            [ fill "#7FD13B"
-            , x "192.99"
-            , y "107.392"
-            , width "107.676"
-            , height "108.167"
-            , transform "matrix(0.7071 0.7071 -0.7071 0.7071 186.4727 -127.2386)"
+        [ g [ transform "scale(1 -1)" ]
+            [ polygon [ fill "rgba(18, 147, 216, 1)", points "-280,-90 0,190 280,-90", transform "translate(0 -210) rotate(0)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 1)", points "-280,-90 0,190 280,-90", transform "translate(-210 0) rotate(-90)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 0.75)", points "-198,-66 0,132 198,-66", transform "translate(207 207) rotate(-45)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 1)", points "-130,0 0,-130 130,0 0,130", transform "translate(150 0) rotate(0)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 0.75)", points "-191,61 69,61 191,-61 -69,-61", transform "translate(-89 239) rotate(0)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 0.75)", points "-130,-44 0,86  130,-44", transform "translate(0 106) rotate(-180)" ] []
+            , polygon [ fill "rgba(18, 147, 216, 0.75)", points "-130,-44 0,86  130,-44", transform "translate(256 -150) rotate(-270)" ] []
             ]
-            []
-        , polygon [ fill "#60B5CC", points "323.298,143.724 323.298,0 179.573,0" ] []
-        , polygon [ fill "#5A6378", points "152.781,161.649 0,8.868 0,314.432" ] []
-        , polygon [ fill "#F0AD00", points "255.522,246.655 323.298,314.432 323.298,178.879" ] []
-        , polygon [ fill "#60B5CC", points "161.649,170.517 8.869,323.298 314.43,323.298" ] []
         ]
+
+
+
+-- polygon fill="" points="" transform=""></polygon></g></svg>
